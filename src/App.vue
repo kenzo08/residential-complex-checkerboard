cb19f1fcb134490969418d774249e7a287cea434
bekhruz <behruzabduvahidov@gmail.com>
2022-08-13 17:45:24 +0500

    Feature: highlight row and col of table

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import FilterBase from "./components/FilterBase.vue";
 
 const entrances = ref([])
 const flats = ref({})
+const houses = ref([])
 
 onMounted(async () => {
   await fetch('/data/data.json')
@@ -14,18 +15,25 @@ onMounted(async () => {
       .then((data) => {
         entrances.value = data.entrances;
         flats.value = data.flats;
+        houses.value = data.houses;
       });
 })
 
 </script>
 
 <template>
-  <div :class="$style.container">
-    <FilterBase/>
-    <Table v-if="entrances.length" :entrances="entrances" :flats="flats" />
+  <div>
+    <FilterBase :class="$style.container"/>
+    <Table v-if="entrances.length" :entrances="entrances" :flats="flats" :houses="houses" />
   </div>
 </template>
 <style module>
 .container{
+}
+</style>
+
+<style module>
+.container{
+  background-color: white;
 }
 </style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="inline-wrapper" v-dragscroll.x>
-    <div v-for="entrance in entrances" class="table-div">
-      <table>
-        <tr v-for="(floor, j) in entrance.floors">
-          <span class="floor">{{ floor.floor }}</span>
-          <td v-for="k in floor.flats">
-            <Flats :flat="getFlat(k.id)"/>
+  <div :class="$style['inline-wrapper']" v-dragscroll.x>
+    <div v-for="entrance in entrances" :key="entrance.id" :class="$style['table-div']">
+      <table :class="$style.table">
+        <tr v-for="floor in entrance.floors" :key="floor">
+          <span :class="$style.floor">{{ floor.floor }}</span>
+          <td v-for="flat in floor.flats" :key="flat.id">
+            <Flats :flat="getFlat(flat.id)"/>
           </td>
         </tr>
       </table>
+      <p :class="$style.title">{{ entrance.title }}</p>
     </div>
   </div>
 </template>
@@ -17,26 +18,28 @@
 import Flats from "./Flats.vue";
 
 interface Props {
+  houses: [];
   entrances: [];
-  flats: object
+  flats: object;
 }
 
-const props = defineProps<Props>()
-const [entrances, flats] = [props.entrances, props.flats]
-
-const getFlat = (id:string) => {
-  return flats[id]
+const props = defineProps<Props>();
+const [entrances, flats] = [props.entrances, props.flats];
+const getFlat = (id: string) => {
+  return flats[id];
 }
 </script>
 
 
-<style scoped>
+<style module>
 .table-div {
   position: relative;
   display: flex;
   align-items: flex-end;
 }
-table {
+
+
+.table {
   border-collapse: separate;
   border-spacing: 2px;
   height: fit-content;
@@ -46,6 +49,7 @@ table {
   font-size: 12px;
   text-align: center;
   vertical-align: center;
+  overflow: hidden;
 }
 
 .floor {
@@ -54,6 +58,7 @@ table {
   text-align: left;
   padding-right: 5px;
 }
+
 .table-div:not(:first-child) {
   margin-left: 20px;
 }
@@ -64,10 +69,42 @@ table {
   overflow-x: scroll;
   -ms-overflow-style: none;
   scrollbar-width: none;
-  padding-bottom: 200px;
+  padding-bottom: 50px;
 }
+
 .inline-wrapper::-webkit-scrollbar {
   display: none;
 }
 
+.table tr:hover {
+  background-color: yellow;
+  cursor: pointer;
+}
+
+td, th {
+  position: relative;
+}
+
+td:hover::after {
+  background-color: yellow;
+  overflow: hidden;
+  content: '';
+  height: 10000px;
+  left: 0;
+  position: absolute;
+  top: -5000px;
+  width: 100%;
+  z-index: -1;
+}
+
+.title{
+  display: flex;
+  flex-direction: row;
+  width: max-content;
+  position: absolute;
+  bottom: -50px;
+  left: 30px;
+  font-size: 13px;
+  font-weight: 600;
+}
 </style>
