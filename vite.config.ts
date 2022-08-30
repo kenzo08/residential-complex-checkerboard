897271bd4b83164cd75589d96594c6765283a306
bekhruz <behruzabduvahidov@gmail.com>
2022-08-30 22:37:09 +0500

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,30 @@ export default defineConfig({
         registerType: 'autoUpdate',
       workbox: {
             clientsClaim: true,
-            skipWaiting: true
+            skipWaiting: true,
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
         },
       devOptions: {
         enabled: true
-      }
+      },
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+            name: 'My Awesome App',
+            short_name: 'MyApp',
+            description: 'My Awesome App description',
+            theme_color: '#ffffff',
+            icons: [
+                {
+                    src: '/img_1.png',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    src: 'img.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                }
+            ]
+        }
     })],
 })
